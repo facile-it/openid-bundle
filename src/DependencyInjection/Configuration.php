d8c4190c69d8242724d43639c904cd2b7d70b36b
Alessandro Lai <alessandro.lai85@gmail.com>
2019-04-29 12:11:57 +0200

    Register controller as a service

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Facile\LoginBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('facile_login');
+
+        return $treeBuilder;
+    }
+}
