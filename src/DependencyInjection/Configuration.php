b4caf37443962714f5c3a3ece80463b0d4c6b551
Alessandro Lai <alessandro.lai85@gmail.com>
2019-04-29 12:13:50 +0200

    Fix root node deprecation handling

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,6 +2,7 @@
 
 namespace Facile\LoginBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -10,6 +11,10 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('facile_login');
+        /** @var ArrayNodeDefinition $rootNode */
+        $rootNode = \method_exists(TreeBuilder::class, 'getRootNode')
+            ? $treeBuilder->getRootNode()
+            : $treeBuilder->root('facile_login');
 
         return $treeBuilder;
     }
